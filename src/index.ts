e90b4157bb0e7a7c14bec8a915e4da7cf7f654a0
sidney011100 <sidney011100@gmail.com>
2025-12-23 23:01:31 +0800

    add route to create order

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+/// <reference path="./types/express/index.d.ts" />
 
 import express from "express";
 import { firebaseAuth } from "./middleware/firebaseAuth";
+import { createOrderController } from "./controllers/orderController";
 
 const app = express();
 app.use(express.json());
@@ -9,6 +11,12 @@ app.use(express.json());
 app.get("/users/:id", firebaseAuth, (req, res) => {
   res.json({ userId: req.user?.uid });
 });
+
+app.get("/hi", (req,res) => {
+  res.json({data: "hello"})
+})
+
+app.post("/order", createOrderController)
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
 
